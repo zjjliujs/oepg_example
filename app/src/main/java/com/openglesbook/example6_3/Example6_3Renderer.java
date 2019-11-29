1106578f366b56139ffead332bbf9bc7e1cad915
LiujinsongRR <liujinsong@rockrobo.com>
2019-11-29 15:43:10 +0800

    glsl script source code extracted to standalone files

--- app/src/main/java/com/openglesbook/example6_3/Example6_3Renderer.java
+++ app/src/main/java/com/openglesbook/example6_3/Example6_3Renderer.java
@@ -38,6 +38,14 @@
 
 package com.openglesbook.example6_3;
 
+import android.content.Context;
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
+
+import com.ljs.android.oepg_ch6.R;
+import com.openglesbook.common.ESShader;
+import com.openglesbook.common.TextResourceReader;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -45,111 +53,79 @@ import java.nio.FloatBuffer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import com.openglesbook.common.ESShader;
+public class Example6_3Renderer implements GLSurfaceView.Renderer {
+    private final Context context;
 
-import android.content.Context;
-import android.opengl.GLES30;
-import android.opengl.GLSurfaceView;
+    private final float[] mVerticesData =
+            {
+                    0.0f, 0.5f, 0.0f, // v0
+                    -0.5f, -0.5f, 0.0f, // v1
+                    0.5f, -0.5f, 0.0f  // v2
+            };
+    // Handle to a program object
+    private int program;
+    // Additional member variables
+    private int mWidth;
+    private int mHeight;
+    private FloatBuffer mVertices;
 
-public class Example6_3Renderer implements GLSurfaceView.Renderer
-{
+    ///
+    // Constructor
+    //
+    public Example6_3Renderer(Context context) {
+        this.context = context;
+        mVertices = ByteBuffer.allocateDirect(mVerticesData.length * 4)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
+        mVertices.put(mVerticesData).position(0);
+    }
 
-   ///
-   // Constructor
-   //
-   public Example6_3Renderer ( Context context )
-   {
+    ///
+    // Initialize the shader and program object
+    //
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        String vertexShaderSrc = TextResourceReader.readTextFileFromResource(context, R.raw.vertex_shader_ch6_3);
 
-      mVertices = ByteBuffer.allocateDirect ( mVerticesData.length * 4 )
-                  .order ( ByteOrder.nativeOrder() ).asFloatBuffer();
-      mVertices.put ( mVerticesData ).position ( 0 );
-   }
+        String fragmentShaderSrc = TextResourceReader.readTextFileFromResource(context, R.raw.fragment_shader_ch6_3);
+        // Load the shaders and get a linked program object
+        program = ESShader.loadProgram(vertexShaderSrc, fragmentShaderSrc);
 
-   ///
-   // Initialize the shader and program object
-   //
-   public void onSurfaceCreated ( GL10 glUnused, EGLConfig config )
-   {
-      String vShaderStr =
-         "#version 300 es                            \n" +
-         "layout(location = 0) in vec4 a_color;      \n" +
-         "layout(location = 1) in vec4 a_position;   \n" +
-         "out vec4 v_color;                          \n" +
-         "void main()                                \n" +
-         "{                                          \n" +
-         "    v_color = a_color;                     \n" +
-         "    gl_Position = a_position;              \n" +
-         "}";
+        GLES30.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    }
 
+    // /
+    // Draw a triangle using the shader pair created in onSurfaceCreated()
+    //
+    public void onDrawFrame(GL10 glUnused) {
+        // Set the viewport
+        GLES30.glViewport(0, 0, mWidth, mHeight);
 
-      String fShaderStr =
-         "#version 300 es            \n" +
-         "precision mediump float;   \n" +
-         "in vec4 v_color;           \n" +
-         "out vec4 o_fragColor;      \n" +
-         "void main()                \n" +
-         "{                          \n" +
-         "    o_fragColor = v_color; \n" +
-         "}" ;
+        // Clear the color buffer
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
 
-      // Load the shaders and get a linked program object
-      mProgramObject = ESShader.loadProgram ( vShaderStr, fShaderStr );
+        // Use the program object
+        GLES30.glUseProgram(program);
 
-      GLES30.glClearColor ( 1.0f, 1.0f, 1.0f, 0.0f );
-   }
+        // Set the vertex color to red
+        GLES30.glVertexAttrib4f(0, 1.0f, 0.0f, 0.0f, 1.0f);
 
-   // /
-   // Draw a triangle using the shader pair created in onSurfaceCreated()
-   //
-   public void onDrawFrame ( GL10 glUnused )
-   {
-      // Set the viewport
-      GLES30.glViewport ( 0, 0, mWidth, mHeight );
+        // Load the vertex position
+        mVertices.position(0);
+        GLES30.glVertexAttribPointer(1, 3, GLES30.GL_FLOAT,
+                false,
+                0, mVertices);
 
-      // Clear the color buffer
-      GLES30.glClear ( GLES30.GL_COLOR_BUFFER_BIT );
+        GLES30.glEnableVertexAttribArray(1);
 
-      // Use the program object
-      GLES30.glUseProgram ( mProgramObject );
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, 3);
 
-      // Set the vertex color to red
-      GLES30.glVertexAttrib4f ( 0, 1.0f, 0.0f, 0.0f, 1.0f );
+        GLES30.glDisableVertexAttribArray(1);
+    }
 
-      // Load the vertex position
-      mVertices.position ( 0 );
-      GLES30.glVertexAttribPointer ( 1, 3, GLES30.GL_FLOAT,
-                                     false,
-                                     0, mVertices );
-
-      GLES30.glEnableVertexAttribArray ( 1 );
-
-      GLES30.glDrawArrays ( GLES30.GL_TRIANGLES, 0, 3 );
-
-      GLES30.glDisableVertexAttribArray ( 1 );
-   }
-
-   ///
-   // Handle surface changes
-   //
-   public void onSurfaceChanged ( GL10 glUnused, int width, int height )
-   {
-      mWidth = width;
-      mHeight = height;
-   }
-
-
-   // Handle to a program object
-   private int mProgramObject;
-
-   // Additional member variables
-   private int mWidth;
-   private int mHeight;
-   private FloatBuffer mVertices;
-
-   private final float[] mVerticesData =
-   {
-      0.0f,  0.5f, 0.0f, // v0
-      -0.5f, -0.5f, 0.0f, // v1
-      0.5f, -0.5f, 0.0f  // v2
-   };
+    ///
+    // Handle surface changes
+    //
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        mWidth = width;
+        mHeight = height;
+    }
 }
